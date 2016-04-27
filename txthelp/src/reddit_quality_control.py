3e82c6288397bbc0372707a85ed8446f81cd1508
gpmo10 <mgaston@seas.upenn.edu>
2016-04-27 12:00:03 -0400

    try now

--- txthelp/src/reddit_quality_control.py
+++ txthelp/src/reddit_quality_control.py
@@ -2,12 +2,12 @@ import praw
 #from ..credentials import app_secret, app_refresh
 
 app_id = 'RcDWOGZcajoyHg'
-app_uri = 'https://127.0.0.1:65010/authorize_callback'
-app_ua = 'description of the text'
+app_uri = 'https://help-text.herokuapp.com/authorize_callback'
+app_ua = 'try something new'
 app_scopes = 'account creddits edit flair history identity livemanage modconfig modcontributors modflair modlog modothers modposts modself modwiki mysubreddits privatemessages read report save submit subscribe vote wikiedit wikiread'
-app_account_code = 'sl5YGDVzOPkolZ7vMi7HB26F0B8'
+app_account_code = 'reWp15hGNfrB6YG_NhDyfi8W4RA'
 
-def login():
+def login2():
     r = praw.Reddit(app_ua)
     r.set_oauth_app_info(app_id, app_secret, app_uri)
     r.refresh_access_information(app_refresh)
@@ -35,7 +35,7 @@ def get_most_upvoted_comment(r, post_id):
 
 
 if __name__ == "__main__":
-    r = login()
+    r = login2()
     top_posts = top_posts(r)
     for post in top_posts:
         comment = get_most_upvoted_comment(r, post.id)
