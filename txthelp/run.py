7580308753f583adde711d636f27342f67e5a416
gpmo10 <mgaston@seas.upenn.edu>
2016-04-27 16:16:25 -0400

    added censoring

--- txthelp/run.py
+++ txthelp/run.py
@@ -45,8 +45,12 @@ def respond():
                 title = message[:100] + "..."
             else:
                 title = message
-
-            message = "<p>" + message + "</p><p><strong> Please help this person answer to this message. Write something useful and no swearing please</strong></p>"
+            '''##Oh I learned how to do this today!
+***
+Let's say you wanted to turn **my post** into the format you use. Well here is *how*:'''
+            
+            message = message + '''
+            **Please help this person answer to this message. Write something useful and no swearing please**'''
             post_id = post_to_reddit(reddit, message, title)
             print 'post_id', '\t', post_id
             activate(from_number, post_id)
